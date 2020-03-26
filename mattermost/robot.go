5185e831be99cac7005d8c97146e3ec98e584e80
winonaso <winonaso@users.noreply.github.com>
2020-03-26 14:18:40 -0400

    Code for randomizing Robot

---
+++ mattermost/robot.go
@@ -0,0 +1,30 @@
+package mattermost
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+func StringifyRobot() (robotString string) {
+	robotEmojis := GetRobotEmojis()
+
+	var emoji1 string
+	var emoji2 string
+
+	rand.Seed(time.Now().UnixNano())
+	emoji1 = robotEmojis[rand.Intn(len(robotEmojis))]
+	rand.Seed(time.Now().UnixNano())
+	emoji2 = robotEmojis[rand.Intn(len(robotEmojis))]
+
+	robotStrings := []string{
+		fmt.Sprintf("%s Beep Boop %s", emoji1, emoji2),
+		fmt.Sprintf("%s Boop %s", emoji1, emoji2),
+		fmt.Sprintf("%s Beep %s", emoji1, emoji2),
+		fmt.Sprintf("%s Boop Beep %s", emoji1, emoji2),
+
+	robotString = robotStrings[rand.Intn(len(robotStrings))]
+
+	return
+}
